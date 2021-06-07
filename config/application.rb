1986ebcec80bc8d7500b04fd0fb2a5c480628fc7
baxix <85516744+baxix@users.noreply.github.com>
2021-06-07 16:06:50 -0300

    Add files via upload

---
+++ config/application.rb
@@ -0,0 +1,35 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module AppHost
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    config.time_zone = 'Beijing'
+    
+    config.i18n.default_locale = "zh-CN"
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    config.eager_load_paths += %W( #{config.root}/lib/parsers #{config.root}/lib #{config.root}/lib/app_host )
+
+    config.autoload_paths += %W( #{config.root}/lib )
+
+  end
+end
+
+module AppHost
+  class << self
+    def version
+      "0.2.0"
+    end
+  end
+end
